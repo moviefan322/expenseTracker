2b17fb3b894cda885a45b843f7dd5c89976c82bb
moviefan322 <philman202@yahoo.com>
2024-11-24 11:11:47 -0500

    adds context as alternative to redux

---
+++ store/expenses-context.tsx
@@ -0,0 +1,78 @@
+import { createContext, useReducer, ReactNode } from "react";
+
+import { ExpenseItem } from "../types/ExpenseItem";
+import { expenses } from "../data/expenses";
+
+interface ExpensesContextType {
+  expenses: ExpenseItem[];
+  addExpense: (expense: ExpenseItem) => void;
+  deleteExpense: (id: string) => void;
+  editExpense: (expense: ExpenseItem) => void;
+}
+
+export const ExpensesContext = createContext<ExpensesContextType>({
+  expenses: [],
+  addExpense: () => {},
+  deleteExpense: () => {},
+  editExpense: () => {},
+});
+
+type Action =
+  | { type: "ADD"; payload: ExpenseItem }
+  | { type: "DELETE"; payload: string }
+  | { type: "EDIT"; payload: ExpenseItem };
+
+type State = ExpenseItem[];
+
+function expensesReducer(state: State, action: Action) {
+  switch (action.type) {
+    case "ADD":
+      return [{ ...action.payload }, ...state];
+    case "DELETE":
+      return state.filter(
+        (expense) => expense.id !== +action.payload
+      );
+    case "EDIT":
+      const updatableExpenseIndex = state.findIndex(
+        (expense: ExpenseItem) => expense.id === action.payload.id
+      );
+      const updatableExpense = state[updatableExpenseIndex];
+      const updatedItem = { ...updatableExpense, ...action.payload };
+      const updatedExpenses = [...state];
+      updatedExpenses[updatableExpenseIndex] = updatedItem;
+      return updatedExpenses;
+    default:
+      return state;
+  }
+}
+
+function ExpensesContextProvider({ children }: { children: ReactNode }) {
+  const [expensesState, dispatch] = useReducer(expensesReducer, expenses);
+
+  const addExpenseHandler = (expense: ExpenseItem) => {
+    dispatch({ type: "ADD", payload: expense });
+  };
+
+  const deleteExpenseHandler = (id: string) => {
+    dispatch({ type: "DELETE", payload: id });
+  };
+
+  const editExpenseHandler = (expense: ExpenseItem) => {
+    dispatch({ type: "EDIT", payload: expense });
+  };
+
+  const value = {
+    expenses: expensesState,
+    addExpense: addExpenseHandler,
+    deleteExpense: deleteExpenseHandler,
+    editExpense: editExpenseHandler,
+  };
+
+  return (
+    <ExpensesContext.Provider value={value}>
+      {children}
+    </ExpensesContext.Provider>
+  );
+}
+
+export default ExpensesContextProvider;
